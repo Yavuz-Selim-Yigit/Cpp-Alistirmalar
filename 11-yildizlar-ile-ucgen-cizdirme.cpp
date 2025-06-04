9b4497dfe7a838d3f2d7319958cae5daa21c8c65
Yavuz Selim Yiğit <yavuzselimyigit06@hotmail.com>
2025-06-04 23:52:18 +0300

    yildizlar-ile-ucgen-cizdirme

---
+++ 11-yildizlar-ile-ucgen-cizdirme.cpp
@@ -0,0 +1,30 @@
+/*
+Kullanıcının girdiği sayıya göre ikizdikkenar üçgen çizen programı yazınız.
+* * * *
+* * *
+* *
+*
+n=4
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    cout << "Kenar uzunlugu: ";
+    cin >> n;
+    m = n;
+
+    for (int i = 1; i <= m; i++)
+    {
+        for (int i = 1; i <= n; i++) // Satır bas
+        {
+            cout << "* ";
+        }
+        cout << endl;
+        n--;
+    }
+
+    return 0;
+}
